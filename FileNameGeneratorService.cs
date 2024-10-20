acbfd4b4e71f14e283c391b03fb225867ca3a0ba
abdu1o <a.kapara2006@gmail.com>
2024-10-20 17:47:31 +0300

    Add project files.

---
+++ FileNameGeneratorService.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace FileNameGenerator
+{
+    public class FileNameGeneratorService
+    {
+        private static readonly char[] AllowedChars = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray();
+
+        public string GenerateFileName(int length)
+        {
+            if (length <= 0) throw new ArgumentException("Length should be greater than 0");
+
+            var fileName = new StringBuilder();
+            var random = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                char randomChar = AllowedChars[random.Next(AllowedChars.Length)];
+                fileName.Append(randomChar);
+            }
+
+            return fileName.ToString();
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿namespace FileNameGenerator
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddSingleton<FileNameGeneratorService>();
+        }
+    }
+}
